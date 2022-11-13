fb184d08a910da0cc4e0e1d64ee8998d08b288c1
Victoria Wu <wuvictoria54@gmail.com>
2022-11-13 18:26:19 -0500

    add copy text

--- app/src/main/java/com/example/cmsc436finalproject/MainActivity.kt
+++ app/src/main/java/com/example/cmsc436finalproject/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.cmsc436finalproject
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.ContentResolver
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import com.google.firebase.provider.FirebaseInitProvider
 import android.graphics.Color
+import android.net.Uri
 import android.view.*
+import androidx.core.content.contentValuesOf
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,11 +38,25 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    val test_text = "testing copy text"
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.copy_text -> Toast.makeText(this, "Copy text selected", Toast.LENGTH_SHORT).show()
-            R.id.copy_image -> Toast.makeText(this, "Copy image selected", Toast.LENGTH_SHORT).show()
-            R.id.save_image -> Toast.makeText(this, "Save image selected", Toast.LENGTH_SHORT).show()
+            R.id.copy_text -> {
+                Toast.makeText(this, "Copy text selected", Toast.LENGTH_SHORT).show()
+
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip: ClipData = ClipData.newPlainText("Copy Translated Text", test_text)
+                clipboard.setPrimaryClip(clip)
+            }
+
+            R.id.copy_image -> {
+                Toast.makeText(this, "Copy image selected", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.save_image -> {
+                Toast.makeText(this, "Save image selected", Toast.LENGTH_SHORT).show()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -45,8 +65,6 @@ class MainActivity : AppCompatActivity() {
         val popup = PopupMenu(this, view)
 
         popup.menuInflater.inflate(R.menu.image_menu, popup.menu)
-        
-
         popup.show()
 
         popup.setOnMenuItemClickListener {
